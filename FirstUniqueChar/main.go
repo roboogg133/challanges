2043f8c96244ac4665c534d3738b6b1ad159b261
roboogg133 <Luizfernandojoaquimdasilva@proton.me>
2025-07-11 16:33:23 -0300

    First Unique Character

---
+++ FirstUniqueChar/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var input string = "aabbccddeefg"
+
+func FirstUniqueChar(CompleteString string) *string {
+
+	NewString := []rune(CompleteString)
+
+	for i := range NewString {
+		if len(NewString) == 0 {
+			break
+		}
+		many := strings.Count(CompleteString, string(NewString[i]))
+		if many > 1 {
+			NewString = []rune(strings.ReplaceAll(CompleteString, string(NewString[i]), ""))
+			NewString = []rune(CompleteString)
+		} else {
+
+			thischar := string(NewString[i])
+			return &thischar
+		}
+	}
+
+	return nil
+}
+
+func main() {
+
+	//	i don't know if the way i did is the best way to do, but it works!
+
+	char := FirstUniqueChar(input)
+
+	if char == nil {
+		fmt.Println("This string don't have a unique char")
+		return
+	}
+
+	fmt.Println(*char)
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # challanges
-post programming challanges.
+You can find programming challanges, in each folder in this repository, try to do yourself with your favorite language
+
+
+# [First Unique Char](#FirstUniqueChar)
+### The challange is:
+Write a function that return the first non repeated char in a string
